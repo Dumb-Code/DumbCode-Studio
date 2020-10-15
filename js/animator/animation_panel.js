9f77be6f817b768750dfed6bcaf5bedf4f99e282
Brandon Davis <neusfear@gmail.com>
2020-10-15 13:15:41 -0500

    animator UI similar to modeler ui

--- js/animator/animation_panel.js
+++ js/animator/animation_panel.js
@@ -4,7 +4,7 @@ export class AnimationPanel {
     constructor(dom) {
         this.editor = dom.find("#editor")
         this.displayDiv = dom.find("#display-div")
-        this.panelHeight = 370
+        this.panelHeight = 340
         this.clickY = 0
         this.updateAreas()
     }
