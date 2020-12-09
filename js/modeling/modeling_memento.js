e8566b28d179c664ebdb824896c9e358e47b7c80
Wyn Price <wynprice999@gmail.com>
2020-12-09 20:46:17 +0000

    Optimized the modeling memento

--- js/modeling/modeling_memento.js
+++ js/modeling/modeling_memento.js
@@ -2,45 +2,30 @@ import { DCMCube } from "../formats/model/dcm_loader.js"
 
 export class ModelingMemento {
     constructor(model) {
-        let writeArray = (arr, len) => {
-            let obj = {}
-            for(let i = 0; i < len; i++) {
-                obj[`e${i}`] = arr[i]
-            }
-            return obj
-        }
         let writeCube = cube => {
             return {
                 name: cube.name,
-                dimension: writeArray(cube.dimension, 3),
-                rotationPoint: writeArray(cube.rotationPoint, 3),
-                offset: writeArray(cube.offset, 3),
-                rotation: writeArray(cube.rotation, 3),
-                textureOffset: writeArray(cube.textureOffset, 2),
+                dimension: [...cube.dimension],
+                rotationPoint: [...cube.rotationPoint],
+                offset: [...cube.offset],
+                rotation: [...cube.rotation],
+                textureOffset: [...cube.textureOffset],
                 textureMirrored: cube.textureMirrored,
-                cubeGrow: writeArray(cube.cubeGrow, 3),
+                cubeGrow: [...cube.cubeGrow],
                 children: cube.children.map(child => writeCube(child))
             }
         }
         this.children = model.children.map(child => writeCube(child))
 
-
-        let readArray = (obj, len) => {
-            let arr = []
-            for(let i = 0; i < len; i++) {
-                arr[i] = obj[`e${i}`]
-            }
-            return arr
-        }
         let readCube = obj => new DCMCube(
             obj.name,
-            readArray(obj.dimension, 3), 
-            readArray(obj.rotationPoint, 3), 
-            readArray(obj.offset, 3), 
-            readArray(obj.rotation, 3),
-            readArray(obj.textureOffset, 2),
+            [...obj.dimension], 
+            [...obj.rotationPoint], 
+            [...obj.offset], 
+            [...obj.rotation],
+            [...obj.textureOffset],
             obj.textureMirrored,
-            readArray(obj.cubeGrow, 3), 
+            [...obj.cubeGrow], 
             obj.children.map(child => readCube(child)),
             model
         )
