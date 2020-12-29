24075e8a16591800d96b35d7642878cd97cc0bce
Wyn Price <wynprice999@gmail.com>
2020-12-29 13:53:25 +0000

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -15,7 +15,7 @@ import { StudioOptions } from "./modeler_options.js";
 
 const major = 0
 const minor = 8
-const patch = 0
+const patch = 1
 
 const version = `${major}.${minor}.${patch}`
 document.getElementById("dumbcode-studio-version").innerText = `v${version}`
