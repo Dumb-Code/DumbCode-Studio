44f8cbc460b8da2cfc3df02cea6a149a915db13a
Brandon Davis <neusfear@gmail.com>
2021-04-30 12:54:32 -0500

    Remove unnecessary stuff and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
       colors: {
         'light-blue': colors.lightBlue,
         cyan: colors.cyan,
