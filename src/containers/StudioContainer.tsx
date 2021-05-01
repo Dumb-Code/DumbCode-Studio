23eb012389870ddc91be9961f30c08ec5477fe19
Brandon Davis <neusfear@gmail.com>
2021-05-01 15:31:50 -0500

    add animations

--- src/containers/StudioContainer.tsx
+++ src/containers/StudioContainer.tsx
@@ -21,7 +21,7 @@ const App = () => {
 
   return (
     <div className="flex flex-col h-screen bg-gray-900 align-middle">
-      <div className="flex flex-row border-b border-gray-100">
+      <div className="flex flex-row border-b border-lightBlue-500">
           <div className="flex-grow">
               {Tabs.map(tab => <NavBarButton key={tab.name} name={tab.name} selected={tab === activeTab} setTab={() => setActiveTab(tab)} />)}
           </div>
@@ -41,7 +41,7 @@ export default App;
 export const NavBarButton = ({name, selected, setTab}: {name: string, selected: boolean, setTab: () => void}) => {
   return (
       <button
-          className={(selected ? "bg-gray-100 text-gray-800" : "bg-gray-700 hover:bg-gray-800 text-gray-300 border-t border-l border-r border-gray-800") + " mt-1 ml-2 hover:text-white rounded-t-xl py-1 px-2"}
+          className={(selected ? "bg-lightBlue-500 text-gray-800" : "bg-gray-700 hover:bg-gray-800 text-gray-300 border-t border-l border-r border-gray-800") + " mt-1 ml-2 hover:text-white rounded-t py-1 px-2"}
           onClick={setTab}
       >
           {name}
--- src/views/project/components/ProjectAnimations.tsx
+++ src/views/project/components/ProjectAnimations.tsx
@@ -1,8 +1,33 @@
 const ProjectAnimations = () => {
     return(
-        <div className="rounded-sm bg-gray-800 h-full">
-            Animations
+        <div className="rounded-sm bg-gray-800 h-full flex flex-col overflow-hidden">
+            <div className="bg-gray-900 text-gray-400 font-bold text-xs p-2 flex flex-row">
+                <p className="flex-grow">ANIMATIONS</p>
+                <p className="text-md">add new icon</p>
+            </div>
+            <div className="border-r border-black flex flex-col overflow-y-scroll h-full w-full pr-6">
+                <AnimationEntry name="roar" selected={true} setRemote={ () => console.log("set remote") } />
+                <AnimationEntry name="walk" selected={true} setRemote={ () => console.log("set remote") } />
+                <AnimationEntry name="bite" selected={false} setRemote={ () => console.log("set remote") } />
+                <AnimationEntry name="poop" selected={true} setRemote={ () => console.log("set remote") } />
+                <AnimationEntry name="eat kash's mom" selected={false} setRemote={ () => console.log("set remote") } />
+                <AnimationEntry name="sniff air" selected={false} setRemote={ () => console.log("set remote") } />
+            </div>
         </div>
+    )
+}
+
+const AnimationEntry = ({name, selected, setRemote}: {name: string, selected: boolean, setRemote: () => void}) => {
+
+    return(
+        <button
+            className={(selected ? "bg-lightBlue-500" : "bg-gray-700") + " my-1 rounded-sm h-10 text-left pl-2 w-full flex flex-row ml-2"}
+            onClick={setRemote}
+        >
+            <p className="flex-grow text-lg mt-1">{name}</p>
+            
+            icons
+        </button>
     )
 }
 
