cff8339e316df089985380daf224a81957e0f5af
Brandon Davis <neusfear@gmail.com>
2021-05-02 22:12:34 -0500

    Add cube properties inputs

--- src/components/CubeInput.tsx
+++ src/components/CubeInput.tsx
@@ -1,7 +1,33 @@
-const CubeInput = () => {
+const CubeInput = ({title}: {title: string}) => {
     return(
         <div>
-            Cube Input
+            <p className="ml-1">{title}</p>
+            <div className="flex flex-col p-1">
+                <InputField axis="x" />
+                <InputField axis="y" />
+                <InputField axis="z" />
+            </div>
+        </div>
+    )
+}
+
+const InputField = ({axis}: {axis: string}) => {
+    
+    let colors = ""
+    switch(axis) {
+        case "x": colors = "bg-red-500"; break;
+        case "y": colors = "bg-green-500"; break;
+        case "z": colors = "bg-lightBlue-500"; break;
+        default: colors = "bg-gray-700"
+    }
+    
+    return(
+        <div className="flex flex-row mb-1 h-7">
+            <div className={colors + " rounded-l px-2 text-white font-bold border-gray-900 pt-1.5"}>
+                {axis.toUpperCase()}
+            </div>
+            <input type="number" className="rounded-r border-none text-xs bg-gray-900 w-full">
+            </input>
         </div>
     )
 }
--- src/views/modeler/components/ModelerProperties.tsx
+++ src/views/modeler/components/ModelerProperties.tsx
@@ -1,11 +1,16 @@
+import CubeInput from "../../../components/CubeInput"
+
 const ModelerProperties = () => {
     return(
         <div className="rounded-sm bg-gray-800 h-full flex flex-col overflow-hidden">
             <div className="bg-gray-900 text-gray-400 font-bold text-xs p-1">
                 <p className="flex-grow my-0.5">CUBE PROPERTIES</p>
             </div>
-            <div className="border-r border-black flex flex-col h-full w-full pr-6">
-                test
+            <div className="border-r border-black h-full w-full grid grid-cols-2 text-gray-600 text-xs">
+                <CubeInput title="DIMENSIONS" />
+                <CubeInput title="POSITIONS" />
+                <CubeInput title="OFFSET" />
+                <CubeInput title="CUBE GROW" />
             </div>
         </div>
     )
