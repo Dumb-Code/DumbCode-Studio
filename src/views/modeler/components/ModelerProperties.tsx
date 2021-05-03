663bba1df9ac2c644e540d4949a5b4f4c57fe0d2
Brandon Davis <neusfear@gmail.com>
2021-05-02 22:33:44 -0500

    adjust some paddings

--- src/views/modeler/components/ModelerProperties.tsx
+++ src/views/modeler/components/ModelerProperties.tsx
@@ -7,13 +7,15 @@ const ModelerProperties = () => {
             <div className="bg-gray-900 text-gray-400 font-bold text-xs p-1">
                 <p className="flex-grow my-0.5">CUBE PROPERTIES</p>
             </div>
-            <div className="border-r border-black h-full w-full grid grid-cols-2">
+            <div className="border-r border-black h-full w-full grid grid-cols-2 px-2 pt-1">
                 <CubeInput title="DIMENSIONS" />
                 <CubeInput title="POSITIONS" />
                 <CubeInput title="OFFSET" />
                 <CubeInput title="CUBE GROW" />
             </div>
-            <CubeRotationInput title="ROTATION" />
+            <div className="px-2">
+                <CubeRotationInput title="ROTATION" />
+            </div>
         </div>
     )
 }
