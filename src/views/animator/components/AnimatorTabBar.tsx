17ec71ba14127c0d09815a0f3f1d3c2b1bf1b6a0
Brandon Davis <neusfear@gmail.com>
2021-05-06 19:23:56 -0500

    idk why this was like that

--- src/views/animator/components/AnimatorTabBar.tsx
+++ src/views/animator/components/AnimatorTabBar.tsx
@@ -15,7 +15,7 @@ const AnimatorTabBar = () => {
 const AnimatorTab = ({name, selected}: {name: string, selected: boolean}) => {
     return(
         <div className={(selected ? "bg-gray-700 text-white hover:bg-gray-600 transform translate-y-0.5 z-50 -mt-0.5" : "bg-gray-900 text-gray-500 hover:bg-black" ) + " w-32 truncate flex flex-row"}>
-            <button className={selected ? "flex-grow mb-2" : "flex-grow mb-2"}>{name}</button>
+            <button className="flex-grow mb-2">{name}</button>
             <button className="bg-gray-800 hover:bg-red-600 rounded pr-0.5 pl-1.5 py-1 my-0.5 w-6 mr-1 hover:text-white text-gray-400 opacity-30 hover:opacity-100"><SVGCross className="h-3 w-3 mr-1" /></button>
         </div>
     )
