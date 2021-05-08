0101773872c5d54f09ca1a2f3798766b0509c44d
Brandon Davis <neusfear@gmail.com>
2021-05-08 18:09:21 -0500

    different animator tab design take 200

--- src/views/animator/components/AnimatorTabBar.tsx
+++ src/views/animator/components/AnimatorTabBar.tsx
@@ -14,9 +14,9 @@ const AnimatorTabBar = () => {
 
 const AnimatorTab = ({name, selected}: {name: string, selected: boolean}) => {
     return(
-        <div className={(selected ? "bg-gray-700 text-white hover:bg-gray-600 transform translate-y-0.5 z-50 -mt-0.5" : "bg-gray-900 text-gray-500 hover:bg-black" ) + " w-32 truncate flex flex-row"}>
-            <button className="flex-grow mb-2">{name}</button>
-            <button className="bg-gray-800 hover:bg-red-600 rounded pr-0.5 pl-1.5 py-1 my-0.5 w-6 mr-1 hover:text-white text-gray-400 opacity-30 hover:opacity-100"><SVGCross className="h-3 w-3 mr-1" /></button>
+        <div className={(selected ? "bg-lightBlue-500" : "bg-gray-900") + " w-32 truncate flex flex-row rounded m-1"}>
+            <button className={(selected ? "text-white" : "text-gray-400") + " flex-grow -mt-1.5"}>{name}</button>
+            <button className="h-4 w-4 bg-gray-800 hover:bg-red-600 rounded pl-0.5 mt-0.5 mr-1 hover:text-white text-gray-400 opacity-30 hover:opacity-100"><SVGCross className="h-3 w-3 mr-1" /></button>
         </div>
     )
 }
