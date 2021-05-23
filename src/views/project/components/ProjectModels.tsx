dba1f9bc7f5991c997d00a0d0b0494e5e72ebc91
Brandon Davis <neusfear@gmail.com>
2021-05-23 14:42:45 -0500

    update download as context and make save button its own thing

--- src/views/project/components/ProjectModels.tsx
+++ src/views/project/components/ProjectModels.tsx
@@ -106,7 +106,6 @@ const ModelEntry = ({ project, selected, changeModel, removeProject }: { project
     }
 
     const linkedToFile = isSaveable && FileSystemsAccessApi
-    const SaveIcon = linkedToFile ? SVGSave : SVGDownload
 
     const isRemote = false
 
@@ -122,31 +121,43 @@ const ModelEntry = ({ project, selected, changeModel, removeProject }: { project
                         <SVGPushGithub className="h-4 w-4 mr-1" />
                     </button>
                 }
-                <button
-                    onClick={e => { saveModel(); e.stopPropagation() }}
-                    className={iconButtonClass + " rounded pr-1 pl-2 py-0.5 my-0.5 mr-1 " + (isModelDirty ? " text-red-600 " : "")}>
-                    <SaveIcon className="h-4 w-4 mr-1" />
-                </button>
+                {linkedToFile &&
+                    <button
+                        onClick={e => { saveModel(); e.stopPropagation() }}
+                        className={iconButtonClass + " rounded pr-1 pl-2 py-0.5 my-0.5 mr-1 " + (isModelDirty ? " text-red-600 " : "")}>
+                        <SVGSave className="h-4 w-4 mr-1" />
+                    </button>
+                }
                 <ContextMenuTrigger id={SAVE_AS_CONTEXT} mouseButton={0}>
                     <div
                         className={iconButtonClass + " rounded pr-1 pl-2 py-0.5 my-0.5 mr-1 " + (isModelDirty ? " text-red-600 " : "")}>
-                        <SVGGrid className="h-4 w-4 mr-1" />
+                        <SVGDownload className="h-6 w-4 mr-1" />
                     </div>
                 </ContextMenuTrigger>
-                <ContextMenu id={SAVE_AS_CONTEXT} className="border border-solid border-black bg-gray-800">
+                <ContextMenu id={SAVE_AS_CONTEXT} className="bg-gray-900 rounded">
+                    <MenuItem>
+                        <div className="p-1 rounded-t w-full bg-gray-700">Download As</div>
+                    </MenuItem>
+                    <MenuItem
+                        onClick={e => { saveModel(); e.stopPropagation() }}
+                    >
+                        <div className="hover:bg-gray-700 cursor-pointer p-1 rounded w-48 m-1 flex flex-row">
+                        <p className="text-gray-300">{project.name.value}</p><p className="text-lightBlue-400 font-bold">.dcm</p>
+                        </div>
+                    </MenuItem>
                     <MenuItem
                         onClick={e => { exportToObj(); e.stopPropagation() }}
                     >
-                        <div className="hover:bg-gray-500 cursor-pointer p-1">
-                            .OBJ
+                        <div className="hover:bg-gray-700 cursor-pointer p-1 rounded w-48 m-1 flex flex-row">
+                            <p className="text-gray-300">{project.name.value}</p><p className="text-lightBlue-400 font-bold">.obj</p>
                         </div>
                     </MenuItem>
                     <MenuItem
                         onClick={e => { exportToGLTF(); e.stopPropagation() }}
 
                     >
-                        <div className="hover:bg-gray-500 cursor-pointer p-1">
-                            .GLTF
+                        <div className="hover:bg-gray-700 cursor-pointer p-1 rounded w-48 m-1 flex flex-row">
+                        <p className="text-gray-300">{project.name.value}</p><p className="text-lightBlue-400 font-bold">.gltf</p>
                         </div>
                     </MenuItem>
                 </ContextMenu>
