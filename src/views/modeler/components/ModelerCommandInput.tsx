d7196681a351045177bd9b3b0633619734001819
Brandon Davis <neusfear@gmail.com>
2021-05-23 20:34:38 -0500

    command feedback light mode fix

--- src/views/modeler/components/ModelerCommandInput.tsx
+++ src/views/modeler/components/ModelerCommandInput.tsx
@@ -11,12 +11,12 @@ const ModelerCommandInput = () => {
                 <input type="text" className="text-xs dark:bg-gray-900 bg-gray-200 dark:text-gray-300 text-black border-none flex-grow focus:outline-none focus:ring-0" placeholder="type your command here" />
             </div>
             {/* Active command feedback */}
-            <div className="relative inline-block z-50 transform translate-x-10 text-xs mt-1 text-white">
+            <div className="relative inline-block z-50 transform translate-x-10 text-xs mt-1 dark:text-white text-black">
                 <p className="underline">command: snap</p>
                 click on first snap point<br/>
             </div>
             {/* Command feedback history */}
-            <div className="tooltip transform translate-x-10 text-xs mt-1 dark:text-gray-400 text-gray-800">
+            <div className="tooltip transform translate-x-10 text-xs mt-1 dark:text-gray-400 text-gray-400">
                 <p className="underline">command: mirror y</p>
                 12 cubes mirrored<br/>
                 <p className="underline">command: refImages</p>
