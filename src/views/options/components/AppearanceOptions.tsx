60369c3985bf1d46490caef248d970698dd9a7fb
Brandon Davis <neusfear@gmail.com>
2021-05-23 21:07:35 -0500

    Add theme color transition

--- src/views/options/components/AppearanceOptions.tsx
+++ src/views/options/components/AppearanceOptions.tsx
@@ -5,19 +5,19 @@ const AppearangeOptions = () => {
 
     return (
         <div className="">
-            <p className="text-black dark:text-white font-semibold mb-2">APPEARANCE OPTIONS</p>
+            <p className="text-black dark:text-white font-semibold mb-2 transition-colors duration-200">APPEARANCE OPTIONS</p>
 
             <p className="text-gray-900 text-xs font-semibold">THEME SELECTION</p>
             <p className="text-gray-900 text-xs mb-2">Allows you to cusomize the interface to your liking.</p>
-            <button className={(!darkMode || "ring-2 ring-lightBlue-500") + " dark:bg-gray-800 bg-gray-300 rounded w-80 dark:text-white text-black font-semibold p-2 text-left pl-4 my-1"} onClick={() => setDarkMode(true)}>Dark Mode</button>
+            <button className={(!darkMode || "ring-2 ring-lightBlue-500") + " transition-colors duration-200 dark:bg-gray-800 bg-gray-300 rounded w-80 dark:text-white text-black font-semibold p-2 text-left pl-4 my-1"} onClick={() => setDarkMode(true)}>Dark Mode</button>
             <br />
-            <button className={(darkMode || "ring-2 ring-lightBlue-500") + " dark:bg-gray-800 bg-gray-300 rounded w-80 dark:text-white text-black font-semibold p-2 text-left pl-4 my-1"} onClick={() => setDarkMode(false)}>Light Mode</button>
+            <button className={(darkMode || "ring-2 ring-lightBlue-500") + " transition-colors duration-200 dark:bg-gray-800 bg-gray-300 rounded w-80 dark:text-white text-black font-semibold p-2 text-left pl-4 my-1"} onClick={() => setDarkMode(false)}>Light Mode</button>
 
             <p className="text-gray-900 text-xs font-semibold mt-4">MODE SELECTION</p>
             <p className="text-gray-900 text-xs mb-2">Allows you to fine tune some of the layouts.</p>
-            <button className={(compactMode || "ring-2 ring-lightBlue-500") + " dark:bg-gray-800 bg-gray-300 rounded w-80 dark:text-white text-black font-semibold p-2 text-left pl-4 my-1"} onClick={() => setCompactMode(false)}>Normal Mode</button>
+            <button className={(compactMode || "ring-2 ring-lightBlue-500") + " transition-colors duration-200 dark:bg-gray-800 bg-gray-300 rounded w-80 dark:text-white text-black font-semibold p-2 text-left pl-4 my-1"} onClick={() => setCompactMode(false)}>Normal Mode</button>
             <br />
-            <button className={(!compactMode || "ring-2 ring-lightBlue-500") + " dark:bg-gray-600 bg-gray-400 rounded w-80 dark:text-gray-800 text-gray-600 font-semibold p-2 text-left pl-4 my-1 cursor-not-allowed"} onClick={() => setCompactMode(true)}>Compact Mode</button>
+            <button className={(!compactMode || "ring-2 ring-lightBlue-500") + " transition-colors duration-200 dark:bg-gray-600 bg-gray-400 rounded w-80 dark:text-gray-800 text-gray-600 font-semibold p-2 text-left pl-4 my-1 cursor-not-allowed"} onClick={() => setCompactMode(true)}>Compact Mode</button>
         </div>
     )
 }
