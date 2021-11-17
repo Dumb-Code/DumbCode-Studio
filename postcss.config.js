565e8f1b0d9dfbb2c8829c09bd0df813a0e5dea4
Wyn Price <wynprice999@gmail.com>
2021-11-17 18:25:50 +0000

    Clean up dependencies and better handling of jit mode

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: { config: './tailwind.config.js' },
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('postcss-preset-env'),
+  ],
 };
