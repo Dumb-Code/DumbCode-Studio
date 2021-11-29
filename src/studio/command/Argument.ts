389f35c010d93f9766682359c2ed4306b5493d3b
Wyn Price <wynprice999@gmail.com>
2021-11-29 01:32:01 +0000

    Made argument parsing reversible

--- src/studio/command/Argument.ts
+++ src/studio/command/Argument.ts
@@ -2,14 +2,15 @@ import { CommandInput } from './CommandInput';
 export class ArgumentHandler<T> {
   private constructor(
     readonly inputParser: (input: CommandInput) => T,
+    readonly toStringFunc: (val: T) => string,
   ) { }
 
-  public static simpleArgument<T>(func: (input: string) => T) {
-    return new ArgumentHandler(input => func(input.getInput()))
+  public static simpleArgument<T>(func: (input: string) => T, toString = (val: T) => String(val)) {
+    return new ArgumentHandler(input => func(input.getInput()), toString)
   }
 
-  public static complexArgument<T>(func: (input: CommandInput) => T) {
-    return new ArgumentHandler(func)
+  public static complexArgument<T>(func: (input: CommandInput) => T, toString: (val: T) => string) {
+    return new ArgumentHandler(func, toString)
   }
 }
 
@@ -32,8 +33,8 @@ const _parseNum = (string: string, integer = false) => {
 }
 export const StringArgument = () => ArgumentHandler.simpleArgument(s => s)
 export const EnumArgument = <T extends string>(...values: readonly T[]) => ArgumentHandler.simpleArgument(s => values[_indexOf(values, s as T)])
-export const IndexArgument = (string: string) => ArgumentHandler.simpleArgument(s => _indexOf(string, s))
-export const IndiciesArgument = (string: string) => ArgumentHandler.simpleArgument(s => [...s.split('')].map(s => _indexOf(string, s)))
+export const IndexArgument = (string: string) => ArgumentHandler.simpleArgument(s => _indexOf(string, s), i => string.charAt(i))
+export const IndiciesArgument = (string: string) => ArgumentHandler.simpleArgument(s => [...s.split('')].map(s => _indexOf(string, s)), is => is.map(i => string.charAt(i)).join(''))
 export const BooleanArgument = () => ArgumentHandler.simpleArgument(s => {
   switch (s) {
     case 'true': return true
@@ -44,10 +45,13 @@ export const BooleanArgument = () => ArgumentHandler.simpleArgument(s => {
 })
 export const NumberArgument = (integer: boolean) => ArgumentHandler.simpleArgument(s => _parseNum(s, integer))
 
-export const AxisArgument = (axis: string, integer = false) => ArgumentHandler.complexArgument(input => {
-  const axisValues = Array.from(input.getInput()).map(s => _indexOf(axis, s))
-  if (input.inputsLeft() > axisValues.length) {
-    throw new Error(`${axisValues.length} axis provided, but only ${input.inputsLeft()} values provided.`);
-  }
-  return axisValues.map(a => ({ axis: a, value: _parseNum(input.getInput(), integer) }))
-})
+export const AxisArgument = (axis: string, integer = false) => ArgumentHandler.complexArgument(
+  input => {
+    const axisValues = Array.from(input.getInput()).map(s => _indexOf(axis, s))
+    if (input.inputsLeft() > axisValues.length) {
+      throw new Error(`${axisValues.length} axis provided, but only ${input.inputsLeft()} values provided.`);
+    }
+    return axisValues.map(a => ({ axis: a, value: _parseNum(input.getInput(), integer) }))
+  },
+  axis => axis.map(a => axis[a.axis]).join("") + axis.map(a => ' ' + a.value).join("")
+)
