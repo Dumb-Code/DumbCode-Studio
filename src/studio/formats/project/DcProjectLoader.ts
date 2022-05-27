366fe290c7ab7dde4beb470c778d684940f7ab83
Wyn Price <wynprice999@gmail.com>
2022-05-27 21:01:04 +0100

    Began dcproj loading

---
+++ src/studio/formats/project/DcProjectLoader.ts
@@ -0,0 +1,99 @@
+import JSZip, { JSZipObject } from "jszip";
+import { imgSourceToElement } from "../../util/Utils";
+import { loadDCMModel } from "../model/OldDCMLoader";
+import DcProject from "./DcProject";
+
+export const loadDcProj = async (name: string, buffer: ArrayBuffer) => {
+  const zip = await JSZip.loadAsync(buffer)
+
+  const model = await loadDcProjModel(zip)
+  const project = new DcProject(name, model)
+
+}
+
+const loadDcProjModel = async (zip: JSZip) => loadDCMModel(file(zip, 'model.dcm').async('arraybuffer'))
+
+
+//Load the texture data. The old structure was as follows:
+//
+//textures             ┌───────────────────────┐
+// ├─ groups.json*     │ MyFirstLayer.png      │ (will be used for 0.png)
+// ├─ texture_names ───┤ Some Second Layer.png │ (will be used for 1.png)
+// ├─ 0.png            │ TheThirdLayer.png     │ (will be used for 2.png)
+// ├─ 1.png            └───────────────────────┘
+// └─ 2.png
+//
+// *groups.json holds an array of elements of the following format:
+// {
+//   name -> a string of the group name
+//   layerIds -> a list of integers pertaining to the layer texture indexes.
+// }
+//
+//Now, it's:
+//textures         
+// ├─ data.json* 
+// ├─ 0.png        
+// ├─ 1.png        
+// └─ 2.png
+//
+// *data.json is of the following format:
+// 
+// {
+//   texture_names: [string],
+//   groups: [{
+//     name: string,
+//     layerIds: [number]
+//   }]
+// }
+
+type TextureData = { texture_names: string[], groups: TextureGroupData[] }
+type TextureGroupData = { name: string, layerIds: number[] }
+
+const loadTextures = async (folder: JSZip, project: DcProject) => {
+  const [textures, datas] = await Promise.all([
+    getTexturesArray(folder),
+    getTexturesData(folder)
+  ])
+
+  textures.forEach((img, index) => {
+    const texture = project.textureManager.addTexture(datas.texture_names[index], img)
+    //TODO: groups
+  })
+
+}
+
+const getTexturesArray = (folder: JSZip) => {
+  const filePromises: Promise<HTMLImageElement>[] = []
+  for (let index = 0, file: JSZipObject | null = null; (file = folder.file(`${index}.png`)) !== null; index++) {
+    const awaitedTexture = file.async('blob').then(texture => imgSourceToElement(URL.createObjectURL(texture)))
+    filePromises.push(awaitedTexture)
+  }
+  return Promise.all(filePromises)
+}
+
+const getTexturesData = (folder: JSZip): Promise<TextureData> => {
+  const dataFile = folder.file("data.json")
+  if (dataFile === null) {
+    return getLegacyTexturesData(folder)
+  }
+  return dataFile.async('text').then(g => JSON.parse(g) as TextureData)
+}
+
+const getLegacyTexturesData = async (folder: JSZip): Promise<TextureData> => {
+  const [groups, textureNames] = await Promise.all([
+    file(folder, "groups.json").async('text').then(g => JSON.parse(g) as TextureGroupData[]),
+    file(folder, "texture_names").async('text').then(s => s.split("\n"))
+  ])
+  return {
+    texture_names: textureNames,
+    groups
+  }
+}
+
+const file = (zip: JSZip, fileName: string) => {
+  const file = zip.file(fileName)
+  if (file === null) {
+    throw new Error(`Unable to find the file '${fileName}'.`)
+  }
+  return file
+}
--- src/studio/util/Utils.ts
+++ src/studio/util/Utils.ts
@@ -7,3 +7,12 @@ export const convertMapToRecord = <V>(map: Map<string, V>): Record<string, V> =>
 export const convertRecordToMap = <V>(dict: Record<string, V>, map = new Map<string, V>()) => {
   Object.keys(dict).forEach(key => map.set(key, dict[key]))
 }
+
+export const imgSourceToElement = (src: string) => {
+  const img = document.createElement("img")
+  return new Promise<HTMLImageElement>((resolve, reject) => {
+    img.onload = () => resolve(img)
+    img.onerror = (e => reject(e))
+    img.src = src
+  })
+}
