3786b781962e85879cb1b42d465adb6e29a4c39a
Wyn Price <wynprice999@gmail.com>
2022-07-06 21:54:08 +0100

    Cleanup texturer viewport

--- src/studio/util/DomParentRef.ts
+++ src/studio/util/DomParentRef.ts
@@ -1,16 +1,19 @@
 import { useEffect, useRef } from 'react';
-export const useDomParent = <T extends HTMLElement>(element: () => HTMLElement) => {
+export const useDomParent = <T extends HTMLElement>(element: () => HTMLElement, allowEmptyRef = false) => {
   const ref = useRef<T>(null)
 
   useEffect(() => {
     if (ref.current === null) {
+      if (allowEmptyRef) {
+        return
+      }
       throw new Error("Ref not set.")
     }
     const cloned = element()
     const currentRef = ref.current
     currentRef.appendChild(cloned)
     return () => { currentRef.removeChild(cloned) }
-  }, [element])
+  }, [element, allowEmptyRef])
 
   return ref
 }
--- src/views/texturer/components/TexturerViewport.tsx
+++ src/views/texturer/components/TexturerViewport.tsx
@@ -1,68 +1,35 @@
-import { useEffect, useRef, useState } from "react"
+import { useState } from "react"
 import StudioCanvas from "../../../components/StudioCanvas"
 import { useStudio } from "../../../contexts/StudioContext"
+import { useDomParent } from "../../../studio/util/DomParentRef"
 
 export const TexturerViewport = () => {
 
-    //Can be "model", "map", or "both"
-    const [viewMode, setViewMode] = useState("both")
+    const [showModel, setShowModel] = useState(true)
+    const [showCanvas, setShowCanvas] = useState(true)
+
+    const { getSelectedProject } = useStudio()
+
+    const ref = useDomParent<HTMLDivElement>(() => getSelectedProject().textureManager.canvas, true)
 
     return (
         <div className="w-full h-full">
-            {viewMode === "both" ? <TexturerBothWindows setStateWith={setViewMode} /> : viewMode === "model" ? <TexturerModelWindow setStateWith={setViewMode} /> : <TexturerTextureWindow setStateWith={setViewMode} />}
-        </div>
-    )
-}
-
-const TexturerTextureWindow = ({ setStateWith }: { setStateWith: any }) => {
-
-    return (
-        <div className="h-full w-full dark:bg-gray-700 bg-gray-200">
-            <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 w-20 rounded-br dark:text-gray-400 text-black pr-1" onDoubleClick={() => { setStateWith("both") }}>Texture</button>
-            <br /><br /><br />
-            <p className="ml-16">putTextureMapHere</p>
-        </div>
-    )
-}
-
-const TexturerModelWindow = ({ setStateWith }: { setStateWith: any }) => {
-
-    return (
-        <div className="h-full w-full dark:border-black border-white border-b">
-            <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 w-20 rounded-br dark:text-gray-400 text-black pr-1" onDoubleClick={() => { setStateWith("both") }}>Model</button>
-            <StudioCanvas />
-        </div>
-    )
-}
-
-const TexturerBothWindows = ({ setStateWith }: { setStateWith: any }) => {
-    const { getSelectedProject } = useStudio()
-
-    const ref = useRef<HTMLDivElement>(null)
-    useEffect(() => {
-        if (!ref.current) {
-            return
-        }
-        const current = ref.current
-        const canvas = getSelectedProject().textureManager.canvas
-
-        current.appendChild(canvas)
-        return () => {
-            current.removeChild(canvas)
-        }
-    })
-
-    return (
-        <div className="flex flex-row h-full">
-            <div className="w-1/2 border-r dark:border-black border-white border-b">
-                <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 w-20 rounded-br dark:text-gray-400 text-black pr-1" onDoubleClick={() => { setStateWith("model") }}>Model</button>
-                <StudioCanvas />
-            </div>
-            <div className=" dark:bg-gray-700 bg-gray-200 w-1/2 border-l dark:border-black border-white">
-                <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 w-20 rounded-br dark:text-gray-400 text-black pr-1" onDoubleClick={() => { setStateWith("texture") }}>Texture</button>
-                <br /><br /><br />
-                <div ref={ref} className="ml-16">{ }</div>
+            <div className="flex flex-row h-full w-full">
+                {showModel && (
+                    <div className="w-full border-r dark:border-black border-white border-b">
+                        <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 w-20 rounded-br dark:text-gray-400 text-black pr-1" onDoubleClick={() => setShowCanvas(!showCanvas)}>Model</button>
+                        <StudioCanvas />
+                    </div>
+                )}
+                {showCanvas && (
+                    <div className=" dark:bg-gray-700 bg-gray-200 w-full border-l dark:border-black border-white">
+                        <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 w-20 rounded-br dark:text-gray-400 text-black pr-1" onDoubleClick={() => setShowModel(!showModel)}>Texture</button>
+                        <br /><br /><br />
+                        <div ref={ref} className="ml-16">{ }</div>
+                    </div>
+                )}
             </div>
         </div>
     )
 }
+
