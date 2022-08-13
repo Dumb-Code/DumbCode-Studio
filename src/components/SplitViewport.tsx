a600b42844390b96f7e4bf449884e578d69992dd
Brandon Davis <neusfear@gmail.com>
2022-08-13 13:43:44 -0500

    Remove hover on viewport labels because bad

--- src/components/SplitViewport.tsx
+++ src/components/SplitViewport.tsx
@@ -13,13 +13,13 @@ export const SplitViewport = ({ children, otherName, selectedCubeHandlerUndoRedo
       <div className="flex flex-row h-full w-full">
         {showModel && (
           <div className="w-full border-r dark:border-black border-white border-b group">
-            <button className="transition-opacity opacity-0 group-hover:opacity-100 dark:bg-gray-900 bg-gray-300 absolute z-10 w-20 rounded-br dark:text-gray-400 text-black pr-1" onDoubleClick={() => setShowOther(!showOther)}>Model</button>
+            <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 px-4 rounded-br dark:text-gray-400 text-black" onDoubleClick={() => setShowOther(!showOther)}>Model</button>
             <StudioCanvas selectedCubeHandlerUndoRedo={selectedCubeHandlerUndoRedo} />
           </div>
         )}
         {showOther && (
           <div className=" dark:bg-gray-700 bg-gray-200 w-full border-l dark:border-black border-white group">
-            <button className="transition-opacity opacity-0 group-hover:opacity-100 dark:bg-gray-900 bg-gray-300 absolute z-10 w-20 rounded-br dark:text-gray-400 text-black pr-1" onDoubleClick={() => setShowModel(!showModel)}>{otherName}</button>
+            <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 px-4 rounded-br dark:text-gray-400 text-black pr-1" onDoubleClick={() => setShowModel(!showModel)}>{otherName}</button>
             {children}
           </div>
         )}
