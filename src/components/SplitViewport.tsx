897651a09cae8d3e887a48692246775ffb86515b
Brandon Davis <neusfear@gmail.com>
2022-08-24 17:07:39 -0500

    On non-desktop screens split viewports vertically

--- src/components/SplitViewport.tsx
+++ src/components/SplitViewport.tsx
@@ -10,15 +10,15 @@ export const SplitViewport = ({ children, otherName, selectedCubeHandlerUndoRedo
 
   return (
     <div className="w-full h-full">
-      <div className="flex flex-row h-full w-full">
+      <div className="flex xl:flex-row flex-col h-full w-full">
         {showModel && (
-          <div className="w-full border-r dark:border-black border-white border-b group">
+          <div className="w-full xl:h-full h-1/2 border-r dark:border-black border-white border-b group">
             <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 px-4 rounded-br dark:text-gray-400 text-black" onDoubleClick={() => setShowOther(!showOther)}>Model</button>
             <StudioCanvas selectedCubeHandlerUndoRedo={selectedCubeHandlerUndoRedo} />
           </div>
         )}
         {showOther && (
-          <div className=" dark:bg-gray-700 bg-gray-200 w-full border-l dark:border-black border-white group">
+          <div className=" dark:bg-gray-700 bg-gray-200 w-full xl:h-full h-1/2 border-l dark:border-black border-white group">
             <button className="dark:bg-gray-900 bg-gray-300 absolute z-10 px-4 rounded-br dark:text-gray-400 text-black" onDoubleClick={() => setShowModel(!showModel)}>{otherName}</button>
             {children}
           </div>
